e60eb983aef44b1bb44165771b6fe9c9b52c1ab8
Abdelaziz k. Shaheen <zezoshaheen22@gmail.com>
2024-03-19 05:21:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Guess a Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🥳 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '☝️ Too HIGH!' : '👇 Too LOW!';
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.guess').value = '';
+    } else {
+      document.querySelector('.message').textContent = '🥲 You LOST!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#ED1D24';
+      document.querySelector('.number').style.width = '30 rem';
+      document.querySelector('.number').textContent = secretNumber;
+      document.querySelector('.guess').value = '';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
